d84a7011151df293010fe76bcb612eea06469fdc
programsec-bud <ljh98@ajou.ac.kr>
2024-02-22 11:44:50 +0900

    new/dev : 1st Commit

---
+++ test/src/main/java/test/test.java
@@ -0,0 +1,18 @@
+package test;
+
+public class test {
+
+	public test() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void newMethod() {
+		System.out.println("new/def 1st commit");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
